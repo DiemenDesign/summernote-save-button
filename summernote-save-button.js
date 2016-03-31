601ad4d85c5428aa8d1cd7edd8a19a0d2d04a6b4
Dennis Suitters <info@studiojunkyard.com>
2016-03-31 19:44:08 +1100

    Added Language, and Options

--- summernote-save-button.js
+++ summernote-save-button.js
@@ -8,14 +8,31 @@
     }
 }
 (function($){
+    $.extend(true,$.summernote.lang,{
+        'en-US':{
+            save:{
+                tooltip:'Save'
+            }
+        }
+    });
+    $.extend($.summernote.options,{
+      save:{
+        icon:'<i class="fa fa-save text-danger"/>'
+      }
+    });
     $.extend($.summernote.plugins,{
         'save':function(context){
             var self=this;
             var ui=$.summernote.ui;
+            var $note=context.layoutInfo.note;
+            var $editor=context.layoutInfo.editor;
+            var $editable=context.layoutInfo.editable;
+            var options=context.options;
+            var lang=options.langInfo;
             context.memo('button.save',function(){
                 var button=ui.button({
-                    contents:'<i class="fa fa-save text-danger"/>',
-                    tooltip:'Save',
+                    contents:options.save.icon,
+                    tooltip:lang.save.tooltip,
                     click:function(){
                         unsaved=false;
                         this.form.submit();
